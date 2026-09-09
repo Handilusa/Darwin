a9a43877b039ab5b97015e6c8233d1b8695b9827
handilusa <miguegofran@gmail.com>
2026-09-09 21:44:30 +0200

    Scripts: add allowance function to erc20Abi

--- scripts/lib/darwin.ts
+++ scripts/lib/darwin.ts
@@ -344,6 +344,7 @@ export const binaryMarketAbi = parseAbi([
 export const erc20Abi = parseAbi([
   "function decimals() view returns (uint8)",
   "function balanceOf(address account) view returns (uint256)",
+  "function allowance(address owner, address spender) view returns (uint256)",
   "function transfer(address to, uint256 amount) returns (bool)",
   "function approve(address spender, uint256 amount) returns (bool)",
   "event Transfer(address indexed from, address indexed to, uint256 value)",
